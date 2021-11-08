6582ef0ed1a257ac0be28a07939d0b8a09c72f90
dansal0807 <danielsaldanha0@gmail.com>
2021-11-08 19:08:10 -0300

    Adicionada a conversão entre moedas.

--- crypto-conversor.py
+++ crypto-conversor.py
@@ -6,11 +6,15 @@ import datetime
 from datetime import date, timedelta
 
 url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+#parâmetros exigidos pela URL do API do coinmarket; significa que ele analise as últimas 100 moedas mais atualizadas e usa o USD como a moeda padrão.""
 parameters = {
   'start':'1',
   'limit':'100',
   'convert':'USD'
 }
+
+#Aqui estão os headers do link, incluindo a chave pessoal de cada desenvolvedor. Por razões de segurança, eu, Daniel Saldanha,
+#escondi minha chave em um arquivo pessoal. Caso deseje utilizar esta API com este código, é necessário criar uma própria.
 headers = {
   'Accepts': 'application/json',
   'X-CMC_PRO_API_KEY': apikey,
@@ -28,6 +32,8 @@ try:
   response = session.get(url, params=parameters)
   data = json.loads(response.text)
 
+#A API do coinmarket devolve um JSON, o que no python é o mesmo que um dicionário e podemos explorá-lo desta forma.
+#Para entender melhor o JSON que a API devolve, é interessante ir no site: https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest
   for entry in data['data']:
     if entry['symbol'] == first_price:
       first_price_num = round(entry['quote']['USD']['price'], 3)
@@ -46,16 +52,6 @@ try:
   qunt_dol = first_price_num*qunt
   qunt_end = round(qunt_dol/second_price_num, 3)
   print(f"... \n {qunt} {first_price} equivalem a: {qunt_dol} USD \n... Sendo 1 {second_price} = {second_price_num}\n {qunt} {first_price} equivale a {qunt_end} {second_price}")
-
-
-
-
-  
-  """print("o preço do bitcoin está:")
-  print((data['data'][0]['symbol']), round(data['data'][0]['quote']['USD']['price'], 2))
-
-  print('o preço do ethereum está:')
-  print((data['data'][1]['symbol']), round(data['data'][1]['quote']['USD']['price'], 2))"""
 
 except (ConnectionError, Timeout, TooManyRedirects) as e:
   print(e)
